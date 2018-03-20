79f5494e637909a262d6c64945b7ad4a2517cd79
Jake Yoon <yjaeseok@gmail.com>
2018-03-20 18:30:39 +0900

    Implement Lab 04 more

---
+++ MLlab/Lab04/1]Hypothesis.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+
+x1_data = [73., 93., 89., 96., 73.]
+x2_data = [80., 88., 91., 98., 66.]
+x3_data = [75., 93., 98., 100., 70.]
+y_data = [152., 185., 180., 196., 142.]
+
+# placeholders for a tensor that with be always fed.
+x1 = tf.placeholder(tf.float32)
+x2 = tf.placeholder(tf.float32)
+x3 = tf.placeholder(tf.float32)
+
+Y = tf.placeholder(tf.float32)
+
+w1 = tf.Variable(tf.random_normal([1]), name='weight1')
+w2 = tf.Variable(tf.random_normal([1]), name='weight2')
+w3 = tf.Variable(tf.random_normal([1]), name='weight3')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
+
+# cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+# minimize. Need a very small learning rate for this data set
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session.
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+for step in range(2001):
+    cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
+                                   feed_dict={x1: x1_data, x2: x2_data, x3: x3_data, Y: y_data})
+    if step % 20 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction: \n", hy_val)
---
+++ MLlab/Lab04/3]LoadDataFromFile.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+
+xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+print(x_data.shape, x_data, len(x_data))
+print(y_data.shape, y_data)
+
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = tf.matmul(X, W) + b
+
+# cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+# minimize. Need a very small learning rate for this data set
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+# Launch the graph in a session.
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+for step in range(20001):
+    cost_val, hy_val, _ = sess.run([cost, hypothesis, train],
+                                   feed_dict={X: x_data, Y: y_data})
+    if step % 20 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction: \n", hy_val)
